f596c9bf515e0fc4ec365d34e2a308c33094851d
ankit <ankit.j.arora@gmail.com>
2017-01-18 18:11:46 +0530

    ability to push profile from inside the content script

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,7 +1,7 @@
 console.log("Extension is active");
 
 var elt = document.createElement("script");
-elt.innerHTML = "window.clevertap = {event:[], profile:[], account:[], notifications:[]};clevertap.account.push({'id': '846-Z7Z-6K4Z'})";
+elt.innerHTML = "window.clevertap = {event:[], profile:[], account:[], notifications:[], onUserLogin:[]};window.clevertap.account.push({'id': '846-Z7Z-6K4Z'})";
 document.head.appendChild(elt);
 
 (function () {
@@ -23,7 +23,35 @@ function raiseEvent(name, properties){
 var eventName = "testevent";
 var property = {
     "property1":"from gmail",
-    "property2":"again from gmail"
+    "property2":"again from gmail",
+    "property3":"again again from gmail"
 };
 
+var user = {
+    "Site": {
+        "Name": "Blah blah",            // String
+        "Identity": 6102,              // String or number
+        "Email": "blah2@gmail.com",         // Email address of the user
+        "Gender": "M",                     // Can be either M or F
+        "Employed": "Y",                   // Can be either Y or N
+        "Education": "Graduate",           // Can be either School, College or Graduate
+        "Married": "Y",                    // Can be either Y or N
+
+// optional fields. controls whether the user will be sent email, push etc.
+        "MSG-email": false,                // Disable email notifications
+        "MSG-push": true,                  // Enable push notifications
+        "MSG-sms": true                    // Enable sms notifications
+    }
+};
+
+
+function raiseOnUserLogin(user){
+    var elt = document.createElement("script");
+    var evtData = 'window.clevertap.onUserLogin.push(' + user + ');';
+    elt.innerHTML = evtData;
+    document.head.appendChild(elt);
+}
+
+
 raiseEvent(eventName, JSON.stringify(property));
+raiseOnUserLogin(JSON.stringify(user));
